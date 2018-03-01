c41666c912430191e2f4ef27bfcf41a050ee26ea
LeonMarchetti <mleonardoa@gmail.com>
2018-03-01 16:49:22 -0300

    tcp_servidor_http.py

--- tcp/tcp_servidor_http.py
+++ tcp/tcp_servidor_http.py
@@ -1,9 +1,10 @@
-from time import localtime, strftime
 from getopt import getopt, GetoptError
+from mimetypes import guess_type
 from os.path import isfile
 from re import match
 import socket
-import sys
+from sys import argv
+from time import localtime, strftime
 
 class ConexionTerminadaExcepcion(Exception):
     pass
@@ -14,28 +15,37 @@ PORT   = 65000
 BUFFER = 1024
 
 def buscar_recurso(recurso):
+
+    # Busco el archivo:
     archivo = 'paginas/' + recurso
     if isfile(archivo):
-        status = b'HTTP/1.0 200 OK\r\n'
+        status = 'HTTP/1.0 200 OK\r\n'
     else:
-        status = b'HTTP/1.0 404 No encontrado\r\n'
+        status = 'HTTP/1.0 404 No encontrado\r\n'
         archivo = 'paginas/no_encontrado.html'
+        
+    # Cabeceras de HTTP:
+    fecha = 'Date: {}\r\n'.format(strftime('%a, %d %b %Y %H:%M:%S %Z', localtime()))
+    tipo_contenido = 'Content-Type: {};charset=utf-8\r\n'.format(guess_type(recurso)[0])
 
-    fecha = 'Date: {}\r\n'.format(strftime('%a, %d %b %Y %H:%M:%S %Z', localtime())).encode()
-    tipo_contenido = b'Content-Type: text/html;charset=utf-8\r\n'
-
+    # Abro el archivo del recurso
     html = b''
     with open(archivo, 'rb') as f:
         for linea in f:
             html += linea
 
-    return status + fecha + tipo_contenido + b'\r\n' + html
+    # Armo la respuesta:
+    return status.encode() + fecha.encode() + tipo_contenido.encode() + b'\r\n' + html
 
 def procesar(pedido):
+
+    # Parseo la primera línea del pedido
     crlf = pedido.find('\r\n')
     primera_linea = pedido[:crlf]
+    
     print('Recibido: "{}"'.format(primera_linea))
 
+    # Busco el recurso y obtengo la respuesta armada
     recurso = match(r'^(GET|POST) \/(.*) HTTP\/(1\.0|1\.1|2\.0)$', primera_linea)
     if recurso:
         return buscar_recurso(recurso.group(2))
@@ -84,7 +94,7 @@ def server():
                 print('Conexión establecida: <{}:{}>'.format(direccion[0], direccion[1]))
 
                 try:
-                    # Recibir petición:
+                    # Recibir pedido:
                     pedido = recibir(cliente)
                     resultado = procesar(pedido)
 
@@ -102,7 +112,7 @@ def server():
 if __name__ == '__main__':
     try:
         # Parámetros de la línea de comandos:
-        opts, _ = getopt(sys.argv[1:], 'i:p:')
+        opts, _ = getopt(argv[1:], 'i:p:')
 
         for opt, arg in opts:
             if opt == '-i': # Dirección IP
@@ -113,4 +123,4 @@ if __name__ == '__main__':
         server()
 
     except GetoptError:
-        print('Error con los parámetros: ' + str(sys.argv))
+        print('Error con los parámetros: ' + str(argv))
