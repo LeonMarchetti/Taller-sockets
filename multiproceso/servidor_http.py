a5b06c4bf3db6072ca48c77363a16e36e6e11e3d
LeonMarchetti <mleonardoa@gmail.com>
2018-03-14 17:51:45 -0300

    servidor_http.py

--- multiproceso/servidor_http.py
+++ multiproceso/servidor_http.py
@@ -17,8 +17,13 @@ class ConexionTerminadaExcepcion(Exception):
 
 
 BUFFER = 1024
-BAD_REQUEST = b'HTTP/1.1 400 Solicitud Incorrecta\r\n\r\n'
+# BAD_REQUEST = b'HTTP/1.1 400 Solicitud Incorrecta\r\n\r\n'
 
+lineas_status = {
+    200: 'HTTP/1.0 200 OK\r\n',
+    400: 'HTTP/1.1 400 Solicitud Incorrecta\r\n',
+    404: 'HTTP/1.0 404 No encontrado\r\n',
+}
 
 def guarderia(signum, frame):
     '''When a child process exits, the kernel sends a SIGCHLD signal. The
@@ -37,77 +42,9 @@ def guarderia(signum, frame):
             return
 
 
-def ejecutar_php(script):
-    p = subprocess.Popen('php ' + script, shell=True, stdout=subprocess.PIPE)
-    return p.stdout.read()
-
-
-def buscar_recurso(recurso, datos):
-    # Busco el archivo:
-    archivo = 'paginas/' + recurso
-
-    tipo_mime = mimetypes.guess_type(recurso)[0]
-    if tipo_mime is None:
-        tipo_mime = 'text/html'
-
-    if os.path.isfile(archivo):
-        status = 'HTTP/1.0 200 OK\r\n'
-    else:
-        status = 'HTTP/1.0 404 No encontrado\r\n'
-        archivo = ('paginas/no_encontrado' +
-                   mimetypes.guess_extension(tipo_mime))
-
-    print('Enviando "{}"'.format(status))
-
-    # Cabeceras de HTTP:
-    fecha = 'Date: {}\r\n'.format(time.strftime('%a, %d %b %Y %H:%M:%S %Z',
-                                                time.localtime()))
-    tipo_contenido = 'Content-Type: {};charset=utf-8\r\n'.format(tipo_mime)
-
-    # Abro el archivo del recurso. Si es un archivo .php entonces lo ejecuto:
-    if os.path.splitext(archivo)[1] == '.php':
-        body = ejecutar_php(archivo)
-    else:
-        body = b''
-        with open(archivo, 'rb') as f:
-            for linea in f:
-                body += linea
-
-    # Armo la respuesta:
-    return (status.encode() +
-            fecha.encode() +
-            tipo_contenido.encode() +
-            b'\r\n' +
-            body)
-
-
-def procesar(mensaje):
-    # Parseo la primera línea del pedido, para obtener tipo de pedido y recurso
-    linea_pedido = mensaje[:mensaje.find('\r\n')]
-
-    print('Recibido: "{}"'.format(linea_pedido))
-
-    pedido = re.match(r'^(GET|POST) \/(.*) HTTP\/(?:1\.0|1\.1|2\.0)$',
-                      linea_pedido)
-
-    if pedido:
-        if pedido.group(1) == 'GET':
-            uri = pedido.group(2)
-            qs = uri.find('?')
-            if qs == -1:
-                return buscar_recurso(uri, '')
-            else:
-                return buscar_recurso(uri[:qs], uri[qs+1:])
-        elif pedido.group(1) == 'POST':
-            linea_vacia = mensaje.find('\r\n\r\n')
-            return buscar_recurso(pedido.group(2), mensaje[linea_vacia+4:])
-        else:
-            return BAD_REQUEST
-    else:
-        return BAD_REQUEST
-
-
 def enviar(s, datos):
+    '''Envía datos a través de un socket.
+    '''
     while datos:
         enviado = s.send(datos)
         if enviado == 0:
@@ -116,6 +53,8 @@ def enviar(s, datos):
 
 
 def recibir(s):
+    '''Recibe un encabezado de http a través de un socket.
+    '''
     cachos = []
     while True:
         cacho = s.recv(BUFFER)
@@ -132,7 +71,83 @@ def recibir(s):
     return ''.join([cacho.decode() for cacho in cachos])
 
 
+def ejecutar_php(script):
+    '''Ejecuta un script de PHP, devolviendo la salida estándar.
+    '''
+    p = subprocess.Popen('php ' + script, shell=True, stdout=subprocess.PIPE)
+    return p.stdout.read()
+
+
+def buscar_recurso(pedido):
+    '''Analiza el pedido y obtiene el recurso a devolver, el código de estado
+       y el tipo mime del recurso.
+    '''
+    if pedido:
+        archivo = 'paginas/' + pedido.group(2)
+        estado = 200
+
+        tipo_mime = mimetypes.guess_type(archivo)[0]
+        if tipo_mime is None:
+            tipo_mime = 'text/plain'
+
+        if not os.path.isfile(archivo):
+            # Página/recurso especial por si no se encuentra el recurso:
+            archivo = 'paginas/no_encontrado' + mimetypes.guess_extension(tipo_mime)
+            estado = 404
+    else:
+        # Página especial por si el mensaje está mal redactado:
+        archivo = 'paginas/bad_request.html'
+        estado = 400
+        tipo_mime = 'text/html'
+        
+    return archivo, estado, tipo_mime
+
+
+def procesar(mensaje):
+    '''Analiza el mensaje HTTP recibido, y devuelve el mensaje de respuesta con
+       el recurso pedido. Si no se encuentra en el servidor se devuelve una
+       página o recurso de no encontrado.
+    '''
+    linea_pedido = mensaje[:mensaje.find('\r\n')]
+    print('Recibido: "{}"'.format(linea_pedido))
+
+    # Parseo el pedido:
+    pedido = re.match(r'^(GET|POST) \/([^?=&\s]*)(?:\?(.*))? HTTP\/(?:1\.0|1\.1|2\.0)$',
+                      linea_pedido)
+    archivo, estado, tipo_mime = buscar_recurso(pedido)
+
+    # Cabeceras de HTTP:
+    linea_status = lineas_status[estado]
+    print('Enviando "{}"'.format(linea_status))
+
+    linea_date = 'Date: {}\r\n'.format(time.strftime('%a, %d %b %Y %H:%M:%S %Z',
+                                                     time.localtime()))
+
+    # Abro el archivo del recurso. Si es un script PHP entonces lo ejecuto:
+    if os.path.splitext(archivo)[1] == '.php':
+        body = ejecutar_php(archivo)
+        tipo_mime = 'text/html'
+    else:
+        body = b''
+        with open(archivo, 'rb') as f:
+            for linea in f:
+                body += linea
+
+    linea_contenttype = 'Content-Type: {};charset=utf-8\r\n'.format(tipo_mime)
+    linea_contentlength = 'Content-Length: {}\r\n'.format(len(body))
+
+    # Armo la respuesta:
+    return (linea_status.encode('ISO-8859-1') +
+            linea_date.encode('ISO-8859-1') +
+            linea_contenttype.encode('ISO-8859-1') +
+            linea_contentlength.encode('ISO-8859-1') +
+            b'\r\n' +
+            body)
+
+
 def server(host, port):
+    '''Atiende pedidos de HTTP, forkeando un subproceso por cada pedido.
+    '''
     try:
         servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         servidor.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -157,8 +172,21 @@ def server(host, port):
             pid = os.fork()
             if pid == 0:
                 servidor.close()
-                hijo(cliente)
-                os._exit(0)
+
+                try:
+                    # Recibe pedido y responde:
+                    pedido = recibir(cliente)
+                    respuesta = procesar(pedido)
+                    enviar(cliente, respuesta)
+
+                except ConexionTerminadaExcepcion:
+                    print('Conexión terminada')
+
+                finally:
+                    print('Cerrando hijo...')
+                    cliente.close()
+                    os._exit(0)
+
             else:
                 print('Conexión establecida: ' +
                       '<{}:{}> con subproceso <{}>'.format(direccion[0],
@@ -172,23 +200,9 @@ def server(host, port):
         servidor.close()
 
 
-def hijo(cliente):
-    try:
-        # Recibir pedido:
-        pedido = recibir(cliente)
-        resultado = procesar(pedido)
-
-        # Enviar resultado:
-        enviar(cliente, resultado)
-
-    except ConexionTerminadaExcepcion:
-        print('Conexión terminada')
-    finally:
-        print('Cerrando hijo...')
-        cliente.close()
-
-
 def main():
+    '''Función principal.
+    '''
     try:
         # Parámetros de la línea de comandos:
         opts, _ = getopt.getopt(sys.argv[1:], 'i:p:')
