704955f0c6be7dc41beee27a26628acca95b0ae5
LeonMarchetti <mleonardoa@gmail.com>
2018-04-05 14:58:55 -0300

    Métodos enviar y recibir arreglados + cambios varios.

--- tcp/tcp_chat_01.py
+++ tcp/tcp_chat_01.py
@@ -11,103 +11,87 @@ import getopt
 import socket
 import sys
 
-# Parametros por defecto para host y puerto:
-BUFFER = 1024
 
 # Mensaje de tamano fijo:
 TAM_MSG = 512
-PADDING = b' '
+PADDING = b'\0'
 
 
-# Comunicación ===============================================================
 class ConexionTerminadaExcepcion(Exception):
     pass
 
 
 def enviar(s, mensaje):
-    b_mensaje = mensaje.ljust(TAM_MSG, PADDING)
+    # Quiebro el mensaje en partes de "TAM_MSG" bytes:
+    paquetes = []
+    while mensaje:
+        paquete = mensaje[:TAM_MSG].ljust(TAM_MSG, PADDING)
+        paquetes.append(paquete)
+        mensaje = mensaje[TAM_MSG:]
 
-    total_enviado = 0
-    while total_enviado < TAM_MSG:
-        enviado = s.send(b_mensaje[total_enviado:])
-        if enviado == 0:
-            raise ConexionTerminadaExcepcion
-        total_enviado += enviado
+    # Envío cada paquete:
+    for paquete in paquetes:
+        while paquete:
+            enviado = s.send(paquete)
+            if enviado == 0:
+                raise ConexionTerminadaExcepcion
 
-
-def enviar_chat(s):
-    mensaje = input('> ')
-
-    if mensaje:
-        enviar(s, mensaje.encode().ljust(TAM_MSG, PADDING))
-
-    else:
-        # Cortar el chat si se ingresa un mensaje vacio.
-        raise ConexionTerminadaExcepcion
+            paquete = paquete[enviado:]
 
 
 def recibir(s):
+    paquetes = []
     cachos = b''
-    bytes_recibidos = 0
-
-    # while bytes_recibidos < TAM_MSG:
-    #     cacho = s.recv(BUFFER)
-    #     if cacho == b'':
-    #         raise ConexionTerminadaExcepcion
-    #
-    #     cachos.append(cacho.strip(PADDING))
-    #     bytes_recibidos += len(cacho)
-
-    while bytes_recibidos != TAM_MSG:
-        try:
-            print('Esperando:')
-            cacho = s.recv(BUFFER)
-            if not cacho:
-                # raise socket.error
-                raise ConexionTerminadaExcepcion
-
+    while True:
+        cacho = s.recv(1024)
+        if cacho:
             cachos += cacho
-            bytes_recibidos += len(cacho)
-            print(f'Recibidos: {len(cacho)} bytes.')
+            while len(cachos) >= TAM_MSG:
+                # Si recibo más de un paquete almaceno el primero, lo saco de
+                # los cachos y sigo recibiendo el próximo.
+                paquetes.append(cachos[:TAM_MSG].strip(PADDING))
+                cachos = cachos[TAM_MSG:]
+                if not cachos:
+                    return paquetes
 
-            if bytes_recibidos > TAM_MSG:
-                # Si recibo más bytes que un solo mensaje, empiezo a contar
-                # los bytes del siguiente mensaje y sigo recibiendo.
-                bytes_recibidos -= TAM_MSG
-
-        except socket.timeout:
-            print('Timeout')
-
-    return cachos.strip(PADDING)
+        else:
+            raise ConexionTerminadaExcepcion
 
 
-def recibir_chat(s):
-    print('< ' + recibir(s).decode())
+def procesar(paquetes):
+    # Parseo del/os paquetes:
+    for paquete in paquetes:
+        print('< {}'.format(paquete.decode()))
 
 
-# Servidor ===================================================================
-def server(direccion):
+def servidor(direccion):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_servidor:
-
         socket_servidor.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         socket_servidor.bind(direccion)
         socket_servidor.listen(5)
-        # socket_servidor.settimeout(0.5)
         print('Escuchando en <{}:{}>'.format(direccion[0], direccion[1]))
 
         while True:  # Ciclo que acepta las conexiones (un cliente a la vez)
-            print('Esperando conexion...')
+            print('--------------------')
+            print('Esperando conexión...')
             socket_cliente, direccion = socket_servidor.accept()
-            # socket_cliente.settimeout(0.5)
-            print('Conexion establecida: <{}:{}>'.format(direccion[0],
-                                                         direccion[1]))
+            print('--------------------')
+            print('Conexión establecida: <{}:{}>\n'.format(direccion[0],
+                                                           direccion[1]))
             try:
                 while True:
-                    recibir_chat(socket_cliente)
-                    # enviar_chat(socket_servidor)
-                    enviar_chat(socket_cliente)
+                    # Recibir mensaje/s:
+                    procesar(recibir(socket_cliente))
 
-            # except socket.error:
+                    # Enviar mensaje:
+                    mensaje = input('> ')
+                    if mensaje:
+                        enviar(socket_cliente, mensaje.encode())
+
+                    else:  # Cortar el chat si se ingresa un mensaje vacio.
+                        print('Chat terminado')
+                        break
+
             except ConexionTerminadaExcepcion:
                 print('Chat terminado')
 
@@ -115,31 +99,35 @@ def server(direccion):
                 socket_cliente.close()
 
 
-# Cliente ====================================================================
-def client(direccion):
+def cliente(direccion):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_servidor:
-        # socket_servidor.settimeout(0.5)
         socket_servidor.connect(direccion)
         try:
             while True:
-                # if not enviar_chat(socket_servidor):
-                #     break
-                enviar_chat(socket_servidor)
-                recibir_chat(socket_servidor)
+                # Enviar mensaje:
+                mensaje = input('> ')
+                if mensaje:
+                    enviar(socket_servidor, mensaje.encode())
 
-        # except socket.error:
+                else:  # Cortar el chat si se ingresa un mensaje vacio.
+                    print('Chat terminado')
+                    break
+
+                # Recibir mensaje:
+                procesar(recibir(socket_servidor))
+
         except ConexionTerminadaExcepcion:
             print('Chat terminado')
 
 
-# ============================================================================
 if __name__ == '__main__':
-    # Parámetros de la línea de comandos:
+    # Parámetros de la linea de comandos:
     try:
         opts, _ = getopt.getopt(sys.argv[1:], 'csi:p:')
     except getopt.GetoptError as error:
         print('Error con el parámetro {0.opt}: {0.msg}'.format(error))
     else:
+        # Parámetros por defecto para host y puerto:
         host = 'localhost'
         port = 10000
         modo = ''
@@ -167,6 +155,6 @@ if __name__ == '__main__':
         if modo == '':
             print('Usar "-c" para modo Cliente y "-s" para modo Servidor')
         elif modo == 's':
-            server((host, port))
+            servidor((host, port))
         elif modo == 'c':
-            client((host, port))
+            cliente((host, port))
