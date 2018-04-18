572d16be00ef1c8217add3a34a296e8ff5f4451d
LeonMarchetti <mleonardoa@gmail.com>
2018-04-18 10:50:20 -0300

    Refactorizado - Ahora el servidor es una clase.

--- select/select_server.py
+++ select/select_server.py
@@ -1,86 +1,110 @@
-# _*_ coding: utf-8 _*_
-"""Servidor que usa la llamada al sistema select."""
+# -*- coding: utf-8 -*-
+"""
+Servidor que usa la llamada al sistema select.
+Fuente: https://pymotw.com/3/select/index.html
+"""
 
 import select
 import socket
 import queue
 
 
-BUFFER = 1024
+class SelectServer:
+    def __init__(self, direccion, proceso):
+        """
+        Inicia el servidor.
+        :param direccion: Par direccion IP y número de puerto asignado al
+        servidor.
+        :param proceso: Funcion que reciba bytes y regrese bytes, para ser
+        ejecutado cada vez que el servidor este listo para procesar los datos
+        de salida.
+        """
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.setblocking(0)
+        self.server.bind(direccion)
+        self.server.listen(5)
+        print('Servidor escuchando en {}:{}...'.format(*direccion))
 
+        self.inputs = [self.server]
+        self.outputs = []
+        self.message_queues = {}
 
-def servidor(direccion, proceso):
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.setblocking(0)
-    server.bind(direccion)
-    server.listen(5)
-    print('Servidor escuchando en {}:{}...'.format(*direccion))
+        while self.inputs:
+            readable, writable, exceptional = select.select(self.inputs,
+                                                            self.outputs,
+                                                            self.inputs)
 
-    inputs = [server]
-    outputs = []
-    message_queues = {}
+            self._handle_inputs(readable)
+            self._handle_outputs(writable, proceso)
+            self._handle_exceptional(exceptional)
 
-    while inputs:
-        readable, writable, exceptional = select.select(inputs, outputs, inputs)
+    def _handle_inputs(self, readable):
+        """The “readable” sockets represent three possible cases. If the
+        socket is the main “server” socket, the one being used to listen for
+        connections, then the “readable” condition means it is ready to
+        accept another incoming connection. In addition to adding the new
+        connection to the list of inputs to monitor, this section sets the
+        client socket to not block. """
+        for readable_socket in readable:
 
-        # Handle inputs
-        for s in readable:
-
-            if s is server:
+            if readable_socket is self.server:
                 # A "readable" socket is ready to accept a connection
-                connection, client_address = s.accept()
-                print('Cliente conectado desde: {}:{}'.format(*client_address))
+                connection, client_address = readable_socket.accept()
+                print('Cliente conectado desde: {}:{}'.format(
+                    *client_address))
 
                 connection.setblocking(0)
-                inputs.append(connection)
+                self.inputs.append(connection)
 
                 # Give the connection a queue for data we want to send
-                message_queues[connection] = queue.Queue()
+                self.message_queues[connection] = queue.Queue()
 
             else:
-                data = s.recv(BUFFER)
+                data = readable_socket.recv(1024)
                 if data:
                     # A readable client socket has data
-                    message_queues[s].put(data)
+                    self.message_queues[readable_socket].put(data)
 
                     # Add output channel for response
-                    if s not in outputs:
-                        outputs.append(s)
+                    if readable_socket not in self.outputs:
+                        self.outputs.append(readable_socket)
 
                 else:
                     # Interpret empty result as closed connection
                     # Stop listening for input on the connection
-                    if s in outputs:
-                        outputs.remove(s)
-                    inputs.remove(s)
-                    s.close()
+                    if readable_socket in self.outputs:
+                        self.outputs.remove(readable_socket)
+
+                    self.inputs.remove(readable_socket)
+                    readable_socket.close()
 
                     # Remove message queue
-                    del message_queues[s]
+                    del self.message_queues[readable_socket]
 
-        # Handle outputs
-        for s in writable:
+    def _handle_outputs(self, writable, proceso):
+        for writable_socket in writable:
             try:
-                next_msg = message_queues[s].get_nowait()
+                next_msg = self.message_queues[writable_socket] \
+                    .get_nowait()
             except queue.Empty:
                 # No messages waiting so stop checking for writability.
-                outputs.remove(s)
-            except KeyError:
-                pass
+                self.outputs.remove(writable_socket)
+            # except KeyError:
+            #     pass
             else:
                 # Ejecución de la función pasada por parámetro, que recibe
                 # los datos recibidos como entrada y devuelve los datos a
                 # enviar como resultado.
                 salida = proceso(next_msg)
-                s.send(salida)
+                writable_socket.send(salida)
 
-        # Handle "exceptional conditions"
-        for s in exceptional:
+    def _handle_exceptional(self, exceptional):
+        for exceptional_socket in exceptional:
             # Stop listening for input on the connection
-            inputs.remove(s)
-            if s in outputs:
-                outputs.remove(s)
-            s.close()
+            self.inputs.remove(exceptional_socket)
+            if exceptional_socket in self.outputs:
+                self.outputs.remove(exceptional_socket)
+            exceptional_socket.close()
 
             # Remove message queue
-            del message_queues[s]
+            del self.message_queues[exceptional_socket]
